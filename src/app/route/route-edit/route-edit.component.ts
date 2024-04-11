c6c98ffa0a65a284efdf07b3857983360ac878b2
Kondziow <konradwojtanowski02@gmail.com>
2024-04-11 09:30:58 +0200

    route edit setup

---
+++ src/app/route/route-edit/route-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-route-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './route-edit.component.html',
+  styleUrl: './route-edit.component.css'
+})
+export class RouteEditComponent {
+
+}
