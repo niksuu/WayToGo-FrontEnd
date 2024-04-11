1bfae75e906a5991695a8288aa579e761c842ef7
Kondziow <konradwojtanowski02@gmail.com>
2024-04-11 13:42:54 +0200

    route.service use backendUrl to build url

--- src/app/route/route.service.ts
+++ src/app/route/route.service.ts
@@ -2,33 +2,33 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Route} from "./route.model";
 import {Page} from "../shared/page.model";
-
+import {backendUrl} from "../shared/http.config";
 
 
 @Injectable({providedIn: 'root'})
 export class RouteService {
 
-  constructor(private http: HttpClient) {
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  getRoutes(pageNumber: number, pageSize: number) {
-    let params = new HttpParams()
-      .set('pageNumber', pageNumber.toString())
-      .set('pageSize', pageSize.toString());
+    getRoutes(pageNumber: number, pageSize: number) {
+        let params = new HttpParams()
+            .set('pageNumber', pageNumber.toString())
+            .set('pageSize', pageSize.toString());
 
-    return this.http.get<Page<Route>>('http://localhost:8090/api/v1/routes', {params});
-  }
+        return this.http.get<Page<Route>>('http://localhost:8090/api/v1/routes', {params});
+    }
 
-  getRouteById(id: string) {
-    const url = `http://localhost:8090/api/v1/routes/${id}`;
-    return this.http.get<Route>(url);
-  }
+    getRouteById(id: string) {
+        const url = `${backendUrl}/routes/${id}`
+        return this.http.get<Route>(url);
+    }
 
-  patchRouteById(id: string, route: Route) {
-    const url = `http://localhost:8090/api/v1/routes/${id}`;
-    return this.http.patch<Route>(
-      url,
-      route
-    );
-  }
+    patchRouteById(id: string, route: Route) {
+        const url = `${backendUrl}/routes/${id}`
+        return this.http.patch<Route>(
+            url,
+            route
+        );
+    }
 }
