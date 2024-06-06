2f599e0498da888d2d0b3106d040be6a5c55bb7f
Janek1010 <janekdomozych99@gmail.com>
2024-06-06 14:39:55 +0200

    FEATURE: isLoggedIn, getUserID

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,8 @@ import {backendUrl} from "../shared/http.config";
 import {Page} from "../shared/page.model";
 import {Route} from "../route/route.model";
 import {Observable, tap} from "rxjs";
+import jwt_decode from "jwt-decode";
+
 
 @Injectable({
   providedIn: 'root'
@@ -30,12 +32,30 @@ export class AuthService {
   getToken(): string | null {
     return localStorage.getItem('jwt_token');
   }
-
-  isLoggedIn(): boolean {
-    return !!this.getToken();
-  }
-
   logout(): void {
     localStorage.removeItem('jwt_token');
   }
+
+  getUserId(): string | null {
+    const token = this.getToken();
+    if (token) {
+      // @ts-ignore
+      const decoded: any = jwt_decode(token);
+      return decoded.userId || null;
+    }
+    return null;
+  }
+  isLoggedIn(): boolean {
+    const token = this.getToken();
+    return token ? !this.isTokenExpired(token) : false;
+  }
+
+  isTokenExpired(token: string): boolean {
+    // @ts-ignore
+    const decoded: any = jwt_decode(token);
+    const currentTime = Math.floor(Date.now() / 1000);
+    return decoded.exp < currentTime;
+  }
+
+
 }
