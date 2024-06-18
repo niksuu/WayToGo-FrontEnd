459fd7ba10ba47cfce95e8158869e75b972ea678
Drillllll <118617644+Drillllll@users.noreply.github.com>
2024-06-18 18:58:28 +0200

    user profile view changed

--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -2,11 +2,16 @@ import {Component, OnInit} from '@angular/core';
 import {User} from "../user.model";
 import {UserService} from "../user.service";
 import {AuthService} from "../../auth/auth.service";
+import { Router, RouterLink} from "@angular/router";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgIf,
+  ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
