7232c8fe14c3b95ea28d60279b04194977df9596
Drillllll <118617644+Drillllll@users.noreply.github.com>
2024-07-03 16:33:08 +0200

    data update instead of navigation

--- src/app/map-location/map-location-list/map-location-list.component.ts
+++ src/app/map-location/map-location-list/map-location-list.component.ts
@@ -211,7 +211,11 @@ export class MapLocationListComponent implements OnInit, OnChanges {
   onMapLocationDelete(mapLocationId: string) {
     if (confirm("You are about to delete map location. Do you want to continue?")) {
       this.mapLocationService.deleteMapLocationFromRoute(mapLocationId, this.route.id).subscribe( () => {
-        this.router.navigate(['yourRoutes/list'])
+        //this.router.navigate(['yourRoutes/list'])
+        this.mapLocations = this.mapLocations.filter(location => location.id !== mapLocationId);
+        this.mapLocationsAndImages = this.mapLocationsAndImages.filter(
+          locationData => locationData.mapLocation.id !== mapLocationId
+        );
       });
     }
   }
