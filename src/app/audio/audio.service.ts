9628c1f57b54c08304ebb8f9a18cd81630e8dbd7
Nikodem Keller <niko.zawodowiec@gmail.com>
2024-08-21 20:30:56 +0200

    osobna lista próby

--- src/app/audio/audio.service.ts
+++ src/app/audio/audio.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpHeaders, HttpParams} from "@angular/common/http";
 import {backendUrl, maxPageSize} from "../shared/http.config";
 import {Page} from "../shared/page.model";
 import {MapLocation} from "../map-location/map-location.model";
-import {catchError, of} from "rxjs";
+import {catchError, Observable, of, throwError} from "rxjs";
 import {Audio} from "./audio.model";
 
 @Injectable({providedIn: 'root'})
@@ -24,6 +24,12 @@ export class AudioService {
   }
 
 
+  getAudioById(audioId: string): Observable<Audio> {
+    const url = `${backendUrl}/audios/${audioId}`;
+    return this.http.get<Audio>(url);
+  }
+
+
   getAudioFileByAudio(audio: Audio) {
     const url = `${backendUrl}/audios/${audio.id}/audio`;
     return this.http.get(url, { responseType: 'blob' }).pipe(
@@ -32,6 +38,49 @@ export class AudioService {
         return of(null);
       })
     );
+  }
+
+  postAudio(audio: Audio) {
+    const url = `${backendUrl}/audios`;
+    return this.http.post<Audio>(url, audio).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error('Error adding audio:', error);
+        throw error; // Przekaż dalej błąd do obsługi w komponencie
+      })
+    );
+  }
+
+  uploadAudioFile(audioId: string, file: File) {
+    const url = `${backendUrl}/audios/${audioId}/audio`;
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+
+    return this.http.post(url, formData, { headers }).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error('Error uploading audio file:', error);
+        throw error; // Przekaż dalej błąd do obsługi w komponencie
+      })
+    );
+  }
+
+  deleteAudio(audioId: string) {
+    const url = `${backendUrl}/audios/${audioId}`;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  updateAudio(audio: Audio): Observable<Audio> {
+    const url = `${backendUrl}/audios/${audio.id}`;
+    return this.http.put<Audio>(url, audio);
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('An error occurred:', error);
+    return throwError('Something bad happened; please try again later.');
   }
 
 
