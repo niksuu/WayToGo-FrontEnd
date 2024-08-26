4b1ac4333c939a8e3728c4b07f072fd1f0c351e1
Kondziow <konradwojtanowski02@gmail.com>
2024-08-26 09:52:56 +0200

    feat: cannot add one point twice to same route

--- src/app/route-map-location/route-map-location.service.ts
+++ src/app/route-map-location/route-map-location.service.ts
@@ -1,14 +1,16 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {backendUrl} from "../shared/http.config";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { backendUrl } from "../shared/http.config";
+import {Observable, of, switchMap} from "rxjs";
+import { map } from "rxjs/operators";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class RouteMapLocationService {
 
   constructor(private http: HttpClient) {}
 
-  postRouteMapLocation(routeId: string, mapLocationId: string) {
-    let newRouteMapLocation = {
+  postRouteMapLocation(routeId: string, mapLocationId: string): Observable<any> | null {
+    const newRouteMapLocation = {
       mapLocation: {
         id: mapLocationId
       },
@@ -16,8 +18,32 @@ export class RouteMapLocationService {
         id: routeId
       },
       sequenceNr: 1
-    }
-    const url = `${backendUrl}/routeMapLocations`
-    return this.http.post(url, newRouteMapLocation)
+    };
+
+    return this.checkIfIsAlreadyCreated(routeId, mapLocationId).pipe(
+      switchMap(isCreated => {
+        if (!isCreated) {
+          const url = `${backendUrl}/routeMapLocations`;
+          return this.http.post(url, newRouteMapLocation);
+        } else {
+          alert('This map Location is already in your route!')
+          return of(null); // Zwraca Observable z wartością `null`, gdy rekord już istnieje
+        }
+      })
+    );
+  }
+
+
+  checkIfIsAlreadyCreated(routeId: string, mapLocationId: string): Observable<boolean> {
+    return this.getRouteMapLocationByMapLocationId(mapLocationId).pipe(
+      map((response: any[]) => {
+        return response.some(location => location.route.id === routeId);
+      })
+    );
+  }
+
+  getRouteMapLocationByMapLocationId(mapLocationId: string): Observable<any> {
+    const url = `${backendUrl}/mapLocation/${mapLocationId}/routeMapLocations`;
+    return this.http.get(url);
   }
 }
